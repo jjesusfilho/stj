ddcc9bb173a05f8aa6eb142f3b2bb5f6b6d33063
José de Jesus Filho <jjesusfilho@gmail.com>
2021-11-18 09:47:31 -0300

    ajuste função stj_baixar_docs_pdf

--- R/stj_baixar_docs_pdf.R
+++ R/stj_baixar_docs_pdf.R
@@ -27,28 +27,37 @@ stj_baixar_docs_pdf <- function(sequencial, registro, data, diretorio  = "."){
 
     u1 <- paste0("https://www.stj.jus.br/websecstj/cgi/revista/REJ.cgi/MON?seq=",x,"&tipo=0&nreg=",y,"&SeqCgrmaSessao=&CodOrgaoJgdr=&dt=", z, "&formato=PDF&salvar=false")
     u2 <- paste0("https://processo.stj.jus.br/processo/dj/documento/?&sequencial=", x,"&num_registro=",y,"&data=",z, "&formato=PDF&componente=MON")
-
+    u3 <- paste0("https://ww2.stj.jus.br/websecstj/cgi/revista/REJ.cgi/ITA?seq=",x,"&tipo=0&nreg=",y, "&SeqCgrmaSessao=&CodOrgaoJgdr=&dt=",z, "&formato=PDF&salvar=false")
     r1 <- httr::GET(u1)
     r2 <- httr::GET(u2)
+    r3 <- curl::curl_fetch_memory(u3)
 
-    rs <- list(r1, r2)
+    rs <- list(r1, r2, r3)
 
     tipo1 <- r1$headers$`content-type`
     tipo2 <- r2$headers$`content-type`
+    tipo3 <- r3$type
 
     pdf <- "application/pdf"
 
-    pdfs <- is.element(c(tipo1, tipo2), pdf)
+    pdfs <- is.element(c(tipo1, tipo2, tipo3), pdf)
 
-    tamanho1 <- r1$headers$`content-length`
-    tamanho2 <- r2$headers$`content-length`
+    tamanho1 <- r1$headers$`content-length` |> as.integer()
+    tamanho2 <- r2$headers$`content-length` |> as.integer()
+    tamanho3 <- r3$headers |>
+               rawToChar() |>
+               stringr::str_squish() |>
+               stringr::str_extract_all("(?<=Content-Length: )\\d+") |>
+               unlist()|>
+               as.integer() |>
+               max()
 
 
 
 
     if (all(pdfs)){
 
-    r <-   rs[which.max(c(tamanho1, tamanho2))][[1]]
+    r <-   rs[which.max(c(tamanho1, tamanho2, tamanho3))][[1]]
 
     } else {
 
